f14dfa52e82dd827cd1ce1a09b36e5e8d73b0615
igoodful <igoodful@qq.com>
2023-04-25 10:16:30 +0800

    Create sysbench_install.sh

---
+++ sysbench_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum -y install make automake libtool pkgconfig libaio-devel openssl-devel mysql-devel
+cd /usr/src/
+wget https://github.com/akopytov/sysbench/archive/refs/tags/1.0.20.tar.gz
+tar xvf 1.0.20.tar.gz
+cd sysbench-1.0.20/
+./autogen.sh
+ ./configure
+make -j 4
+make install
