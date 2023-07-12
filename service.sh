2f59e4b7741ee4b4a5a159eb1f36bb302c99f9b6
igoodful <igoodful@qq.com>
2023-07-12 15:53:42 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,7 +1,8 @@
 
 ntpdate_start(){
+  sed -i -e '/^server/s/^/#/' -e '1a server ntp.aliyun.com iburst' /etc/chrony.conf
+  systemctl restart chronyd.service
   systemctl enable chronyd.service
-  systemctl start chronyd.service
 
 }
 
