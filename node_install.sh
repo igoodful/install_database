3333baf7b95b6bf4ccab2c8cb861c3d07e870ae2
igoodful <igoodful@qq.com>
2023-07-17 21:36:19 +0800

    node update

--- node_install.sh
+++ node_install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-node_version='12.16.3'
+node_version='14.8.0'
 node_package_dir="node-v${node_version}-linux-x64"
 node_package_targz="${node_package_dir}.tar.gz"
-function install(){
-wget https://nodejs.org/dist/v${node_version}/${node_package_targz}
-tar -xzvf ${node_package_targz}
-ln -s   /usr/bin/node
-ln -s   /usr/bin/npm
+install_dir="/data/node-${node_version}"
+mkdir -p $install_dir
 
-
+function install() {
+	wget https://nodejs.org/dist/v${node_version}/${node_package_targz}
+	tar -xzvf ${node_package_targz}
+	mv node-v${node_version}-linux-x64/* ${install_dir}/
+	if [ -f ${install_dir}/bin/npm ]; then
+		rm -rf /usr/bin/node
+		rm -rf /usr/bin/npx
+		rm -rf /usr/bin/npm
+		ln -s ${install_dir}/bin/node /usr/bin/node
+		ln -s ${install_dir}/bin/npm /usr/bin/npm
+		ln -s ${install_dir}/bin/npx /usr/bin/npx
+		echo "install ok"
+	else
+		echo "install fail"
+		exit 1
+	fi
 
 }
 
+install
